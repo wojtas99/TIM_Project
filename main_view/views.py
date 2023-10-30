557106e3aa63592c5df756ec45812331c9a2337f
wojtas99 <wojciechkraw99@gmail.com>
2023-10-30 15:11:07 +0100

    BackEnd

--- main_view/views.py
+++ main_view/views.py
@@ -1,34 +1,27 @@
-from rest_framework.views import APIView
-from .serializer import *
+from django.contrib.auth import authenticate, login
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework import generics, status
+from rest_framework.views import APIView
 from django.http import HttpResponse
-from django.contrib.auth import authenticate
-from django.shortcuts import redirect
-from rest_framework.authtoken.models import Token
-from django.http import HttpResponse
+from .serializer import *
 
 
 def index(request):
     return HttpResponse("Succes Login")
 
 
-
 class Login(APIView):
-    serializer_class = UserSerializer
-
     def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            login = serializer.validated_data['login']
-            password = serializer.validated_data['password']
-            user = authenticate(request, username=login, password=password)
-            if user is not None:
-                token, created = Token.objects.get_or_create(user=user)
-                return Response({'token': token.key})
-            else:
-                return Response({'error': 'Invalid login credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        username = request.data.get('login')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response({'message': 'Zalogowano pomyślnie'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Nieprawidłowe dane logowania'}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 class Register(APIView):
