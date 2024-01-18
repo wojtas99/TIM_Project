3f7b778d741624e81e922b9e49de4f1543937643
wojtas99 <wojciechkraw99@gmail.com>
2024-01-18 21:53:17 +0100

    18.01.2024r.

--- main_view/models.py
+++ main_view/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 
-fields = ['id', 'group_name', 'trainer_name', 'max_size', 'actual_size']
-
 
 class sportGroups(models.Model):
-    id = models.IntegerField(primary_key=True)
-    discipline = models.CharField(max_length=50, null=True, blank=True)
-    trainer_name = models.CharField(max_length=20, null=True, blank=True)
-    max_size = models.IntegerField(default=0)
+    discipline = models.CharField(max_length=50, default="")
+    trainer_name = models.CharField(max_length=50, default="")
     actual_size = models.IntegerField(default=0)
+    max_size = models.IntegerField(default=0)
 
     class Meta:
         db_table = "sportGroups"
