941ed5cf2e6f5eb2e0fc47e77a3379e0564c534a
wojtas99 <wojciechkraw99@gmail.com>
2024-01-19 12:52:25 +0100

    19.01.2024r. BackEnd

--- main_view/models.py
+++ main_view/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
 
-
-
-
 class sportGroups(models.Model):
     discipline = models.CharField(max_length=50, default="")
     trainer_name = models.CharField(max_length=50, default="")
     actual_size = models.IntegerField(default=0)
     max_size = models.IntegerField(default=0)
+    start_date = models.DateField(null=True, blank=True)
+    start_time = models.TimeField(null=True, blank=True)
 
     class Meta:
         db_table = "sportGroups"
