d6d1354fbb4556e765aedefeca0bf26d5441a190
Krzysztof <krzysztof.maj01@interia.pl>
2024-01-24 09:56:46 +0100

    FrontEnd

--- frontend/src/Groups/GroupInvite.jsx
+++ frontend/src/Groups/GroupInvite.jsx
@@ -1,12 +1,36 @@
 import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom"; 
 import TopBar from "../TopBar";
 
 const GroupInvite = () => {
   const [isAnimated, setIsAnimated] = useState(false);
+  const [groupId, setGroupId] = useState(null);
+  const navigate = useNavigate();
+  const { id } = useParams();
 
   useEffect(() => {
+    setGroupId(id);
     setIsAnimated(true);
-  }, []);
+  }, [id]);
+
+  const handleJoinUsClick = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/group/${groupId}`, {
+        method: 'PUT',
+      });
+
+      if (response.ok) {
+        alert('Dołączono do grupy!');
+        navigate('/dashboard'); // Używamy navigate zamiast history.push
+      } else {
+        alert('Błąd podczas dołączania do grupy');
+      }
+    } catch (error) {
+      console.error('Wystąpił błąd:', error);
+    }
+  };
+
+
 
   return (
     <div className="dashboard-page">
@@ -14,7 +38,7 @@ const GroupInvite = () => {
       <div className="create-page">
         <div className={`create-group-form ${isAnimated ? "animate" : ""}`}>
           <p>You have chosen Your group, if You want to attend on a meeting, click JOIN US!!</p>
-          <buttonCreate>JOIN US!</buttonCreate>
+          <buttonCreate onClick={handleJoinUsClick}>JOIN US!</buttonCreate>
         </div>
       </div>
     </div>
